3090372f141ac8b338c7456a0eb5922f1ef20eaf
Mohit Soni <154327511+mohitsoniv@users.noreply.github.com>
2024-12-24 19:43:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,74 @@
+# Create S3 Bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket = "mohit-tf-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "error.html"
+  source = "error.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "styles" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "css/styles.css"
+  source = "css/styles.css"  # Ensure this matches the actual location of styles.css
+  acl    = "public-read"
+  content_type = "text/css"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+    bucket = aws_s3_bucket.bucket.id
+    index_document {
+      suffix = "index.html"
+    }
+  
+    error_document {
+      key = "error.html"
+    }
+    
+    depends_on = [ aws_s3_bucket_acl.example ]
+  }
